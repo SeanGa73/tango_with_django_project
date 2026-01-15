198eb7b62ef4f49db8943d1c55384f812f818fdc
Sean Galloway <3016924g@student.gla.ac.uk>
2026-01-15 15:32:33 +0000

    testing git setup

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
+""" testing git, with a comment"""
+
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
