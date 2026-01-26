a78092fa1180dc194a05eb5c5b4fa49f86d28d2a
Sean Galloway <3016924g@student.gla.ac.uk>
2026-01-26 19:18:22 +0000

    Chapter 5 and 6

--- populate_rango.py
+++ populate_rango.py
@@ -6,6 +6,7 @@ import django
 django.setup()
 from rango.models import Category, Page
 
+
 def populate():
     python_pages = [{'title':'Official Python Tutorial', 'url':'http://docs.python.org/3/tutorial/'}
                     , {'title': 'How to Think like a Computer Scientist', 'url': 'http://www.greenteapress.com/thinkpython/'}, 
@@ -22,29 +23,29 @@ def populate():
     ]
 
     cats = {
-        'Python': {'pages': python_pages}, 
-        'Django': {'pages': django_pages}, 
-        'Other Frameworks': {'pages': other_pages}, 
+        'Python': {'pages': python_pages, 'views': 128,'likes': 64}, 
+        'Django': {'pages': django_pages, 'views': 64,'likes': 32}, 
+        'Other Frameworks': {'pages': other_pages, 'views': 32,'likes': 16}, 
     }
 
     for cat, cat_data in cats.items():
-        c = add_cat(cat)
+        c = add_cat(cat, cat_data['views'], cat_data['likes'])
         for p in cat_data['pages']:
             add_page(c, p['title'], p['url'])
     
     for c in Category.objects.all():
-        for p in Page.objects.filter(Category = c):
+        for p in Page.objects.filter(category = c):
             print(f'- {c}: {p}')
 
 def add_page(cat, title, url, views =0):
-    p = Page.objects.get_or_create(Category = cat, title = title)[0]
+    p = Page.objects.get_or_create(category = cat, title = title)[0]
     p.url = url
     p.views = views
     p.save()
     return p
 
-def add_cat(name):
-    c = Category.objects.get_or_create(name = name)[0]
+def add_cat(name, views = 0, likes = 0):
+    c = Category.objects.get_or_create(name = name, views = views, likes = likes)[0]
     c.save()
     return c
 
