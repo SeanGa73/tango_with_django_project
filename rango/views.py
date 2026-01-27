ba094ce8b7cb7bc93696591c6d33ad2b9cb9f5b5
Sean Galloway <3016924g@student.gla.ac.uk>
2026-01-27 17:29:45 +0000

    Chapter 6

--- rango/views.py
+++ rango/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from rango.models import Category
+from rango.models import Page
 
 
 
@@ -8,13 +9,28 @@ def index(request):
     # context_dict = {'boldmessage':'this tutorial has been put together by Sean' }
     # // this might be thw wrong way round i.e. ^ the above message should be for about 
     category_list = Category.objects.order_by('-likes')[:5]
+
+    most_views = Page.objects.order_by('-views')[:5]
     
     context_dict = {}
     context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
     context_dict['categories'] = category_list
+    context_dict['pages'] = most_views
     
     return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
     context_dict = {'boldmessage':'This tutorial has been put together by Sean' }
     return render(request, 'rango/about.html', context=context_dict)
+
+def show_category(request, category_name_slug):
+    context_dict = {}
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+        pages = Page.objects.filter(category=category)
+        context_dict['pages'] = pages
+        context_dict['category'] = category
+    except Category.DoesNotExist:
+        context_dict['pages'] = None
+        context_dict['category'] = None
+    return render(request, 'rango/category.html', context=context_dict)
